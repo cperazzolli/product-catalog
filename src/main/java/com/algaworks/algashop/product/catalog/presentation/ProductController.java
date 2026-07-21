0df9a2cb7637b290292415421400cbbbca46883d
Claudinei Perazzolli <claudineii.perazzolli@gmail.com>
2026-07-20 22:29:41 -0300

    Projeto pronto com stubs

--- src/main/java/com/algaworks/algashop/product/catalog/presentation/ProductController.java
+++ src/main/java/com/algaworks/algashop/product/catalog/presentation/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -48,6 +50,51 @@ public class ProductController {
                         .id(UUID.randomUUID())
                         .name("Notebook")
                         .build())
+                .build();
+    }
+
+    @GetMapping
+    public PageModel<ProductDetailOutput> filter(
+            @RequestParam(name = "size",required = false) Integer size,
+            @RequestParam(name = "number",required = false) Integer number
+    ) {
+        return PageModel.<ProductDetailOutput>builder()
+                .number(0)
+                .size(size)
+                .totalPages(1)
+                .totalElements(1)
+                .content(
+                        List.of(ProductDetailOutput.builder()
+                                .id(UUID.randomUUID())
+                                .addedAt(OffsetDateTime.now())
+                                .name("Notebook X11")
+                                .brand("Deep Driver")
+                                .regularPrice(BigDecimal.valueOf(1500.00))
+                                .salePrice(BigDecimal.valueOf(1000.00))
+                                .inStock(true)
+                                .enabled(true)
+                                .description("A Gamer Notebook")
+                                .category(CategoryMinimalOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .name("Notebook")
+                                        .build())
+                                .build(),
+                        ProductDetailOutput.builder()
+                                .id(UUID.randomUUID())
+                                .addedAt(OffsetDateTime.now())
+                                .name("Desktop I9000")
+                                .brand("Deep Driver")
+                                .regularPrice(BigDecimal.valueOf(3500.00))
+                                .salePrice(BigDecimal.valueOf(3000.00))
+                                .inStock(false)
+                                .enabled(true)
+                                .description("A Gamer Notebook")
+                                .category(CategoryMinimalOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .name("Desktop")
+                                        .build())
+                                .build()
+                ))
                 .build();
     }
 }
